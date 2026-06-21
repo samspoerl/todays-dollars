72897478c9e47f1cc1220515e7b795f10f6857ae
Sam Spoerl <sgspoerl@outlook.com>
2026-06-21 15:07:02 -0400

    chore(deps): migrate from individual @radix-ui/* packages to unified radix-ui
    Closes #65

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu"
+import { NavigationMenu as NavigationMenuPrimitive } from "radix-ui"
 import { cva } from "class-variance-authority"
 import { ChevronDownIcon } from "lucide-react"
 
